a5d8af49fae7c72e5b0d095c3c24a2ffab1af69a
rahul202224 <rahuulraj224@gmail.com>
2026-06-05 22:14:01 +0530

    Create longest_prefix.cpp

---
+++ week_1/day-5/longest_prefix.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public:
+    string lCP(vector<string>& strs) 
+      {
+        string k = strs[0];
+
+        for(int i=1;i<strs.size();i++) 
+        {
+            while(strs[i].find(k) != 0)         
+            {
+                ans.pop_back();
+            }
+        }
+
+        return ans;
+      }
+};
